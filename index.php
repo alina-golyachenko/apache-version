559a9c8bfb620bd26cbecc956e0db011d2044cbd
Alina Golyachenko <ipz202_gaa@student.ztu.edu.ua>
2022-04-10 00:06:28 +0200

    testing core initialization

--- index.php
+++ index.php
@@ -1,14 +1,25 @@
 <?php
-echo "hi <3"
+//echo "hi <3"
+//?>
+<!--<!DOCTYPE html>-->
+<!--    <html lang="en">-->
+<!--    <head>-->
+<!--        <link rel="stylesheet" href="CSS/main.css">-->
+<!--        <meta charset="UTF-8">-->
+<!--        <title>Witch's kitchen</title>-->
+<!--    </head>-->
+<!--    <body>-->
+<!--    <img src="Files/Images/apple%20pie.jpg">-->
+<!--    </body>-->
+<!--    </html>-->
+<?php
+
+include('Config/config.php');
+include('Core/Core.php');
+
+$core = \Core\Core::getInstance();
+$core -> init();
+$core -> run();
+$core -> done();
+
 ?>
-<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <link rel="stylesheet" href="CSS/main.css">
-        <meta charset="UTF-8">
-        <title>Witch's kitchen</title>
-    </head>
-    <body>
-    <img src="Files/Images/apple%20pie.jpg">
-    </body>
-    </html>
